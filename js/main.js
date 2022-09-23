5733fd3f65e80c0dccfbc49a19de65de493353c4
LudovDI <dima-l-01@mail.ru>
2022-09-23 23:02:40 +0300

    hgvjhv

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function randomInteger(min, max) {
+    if (min < 0 || max < 0 || max <= min) {
+        return null;
+    }
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+function checkForMaxLength(testString, maxLength) {
+    return testString.legth <= maxLength;
+}
