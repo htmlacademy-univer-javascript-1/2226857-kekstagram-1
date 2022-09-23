727e8fb9cecd1398ae714d8038494ad62b481b18
LudovDI <dima-l-01@mail.ru>
2022-09-23 23:35:59 +0300

    lesson2_2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function randomInteger(min, max) {
-    if (min < 0 || max < 0 || max <= min) {
-        return null;
-    }
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+  if (min < 0 || max < 0 || max <= min) {
+    return null;
+  }
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
 }
 
 function checkForMaxLength(testString, maxLength) {
-    return testString.legth <= maxLength;
+  return testString.legth <= maxLength;
 }
