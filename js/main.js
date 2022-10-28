681ea3a4cf709f0e527b13dbef68c388596eed6d
LudovDI <dima-l-01@mail.ru>
2022-10-28 17:05:26 +0300

    task2_17 corrected#2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // https://learn.javascript.ru/task/random-int-min-max
+// eslint-disable-next-line no-unused-vars
 function randomInteger(min, max) {
   if (min < 0 || max < 0 || max <= min) {
     return null;
   }
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
+// eslint-disable-next-line no-unused-vars
 function checkForMaxLength(testString, maxLength) {
   return testString.legth <= maxLength;
 }
