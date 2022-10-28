158b94b5fc842936cdbe987982711f7d02f6c46a
LudovDI <dima-l-01@mail.ru>
2022-10-28 19:37:40 +0300

    task7

--- js/util.js
+++ js/util.js
@@ -3,10 +3,11 @@ function randomInteger(min, max) {
   if (min < 0 || max < 0 || max <= min) {
     return null;
   }
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
+// eslint-disable-next-line no-unused-vars
 function checkForMaxLength(testString, maxLength) {
   return testString.legth <= maxLength;
 }
