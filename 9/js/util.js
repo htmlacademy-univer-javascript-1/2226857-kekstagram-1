e88f350209e971f03c33f81db1c462ab07898de5
Keks <keks@htmlacademy.ru>
2022-11-03 21:04:20 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,15 @@
+// https://learn.javascript.ru/task/random-int-min-max
+function randomInteger(min, max) {
+  if (min < 0 || max < 0 || max <= min) {
+    return null;
+  }
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+// eslint-disable-next-line no-unused-vars
+function checkForMaxLength(testString, maxLength) {
+  return testString.legth <= maxLength;
+}
+
+export {randomInteger};
