e2384457d9aeddc5ba549428c5dd27e0bb0fedf3
LudovDI <dima-l-01@mail.ru>
2022-12-11 21:30:27 +0300

    module12task1

--- js/main.js
+++ js/main.js
@@ -2,10 +2,21 @@ import './drawThumbnail.js';
 import './drawFullSize.js';
 import './validForm.js';
 import './imgForm.js';
-import {createPictures} from './drawThumbnail.js';
+import {createPictures, setDefaultClick, setDiscussedClick, setRandomClick} from './drawThumbnail.js';
 import {getData} from './api.js';
-import { showAlert } from './util.js';
+import { debounce, showAlert } from './util.js';
+
 
 getData((pictures) => {
-  createPictures(pictures);
+  createPictures(pictures, 'default');
+  setDefaultClick(debounce(
+    () => createPictures(pictures, 'default'),
+  ));
+  setDiscussedClick(debounce(
+    () => createPictures(pictures, 'discussed'),
+  ));
+  setRandomClick(debounce(
+    () => createPictures(pictures, 'random'),
+  ));
 }, showAlert);
+document.querySelector('.img-filters').classList.remove('img-filters--inactive');
